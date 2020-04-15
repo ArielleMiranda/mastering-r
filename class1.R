262a67e3cb2df525d88dbb75670720691ecbd722
arielle-rserver <arielle-rserver>
2020-04-15 12:40:33 +0000

    update solution

--- class1.R
+++ class1.R
@@ -1,26 +1,17 @@
 library(binancer)
 library(data.table)
 library(jsonlite)
-library(httr)
+library(logger)
 
-# 1. We have 0.42 Bitcoin. Let's write an R script reporting on the current value of this asset in USD.
-a <- as.data.table(binance_coins_prices(unit = "USDT"))
-a[symbol =='BTC', 'usd'] * 0.42
+#### push to github
 
-# 2. Report on the current price of 0.42 BTC in HUF
-## solution 1: run in terminal: GET https://api.exchangeratesapi.io/latest?base=USD HTTP/1.1
-a[symbol =='BTC', 'usd'] * 0.42 * 350.7
+BITCOINS <- 0.42
+log_info('Number of Bitcoins: {BITCOINS}')
 
-## solution 2: 
-resp<-GET("https://api.exchangeratesapi.io/latest?base=USD")
-exch_rates<-content(resp,as="parsed") 
-exch_rates$rates$HUF
-a[symbol =='BTC', 'usd'] * 0.42 * exch_rates$rates$HUF
-
-## solution 3: 
 usdhuf <- fromJSON('https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF')$rates$HUF
-0.42 * binance_coins_prices()[symbol == 'BTC', usd] * usdhuf
+log_info('The value of 1 USD in HUF: {usdhuf}')
 
-## solution 4: 
-0.42 * binance_coins_prices()[symbol == 'BTC', usd] * fromJSON('https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF')$rates$HUF
+btcusdt <-  binance_coins_prices()[symbol == 'BTC', usd]
+log_info('The value of 1 Bitcoin in USD: {btcusdt}')
 
+BITCOINS * btcusdt * usdhuf
